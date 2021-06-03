55316c2b1df1fd9a4bcbcfe7e8537da29facbd80
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-06-03 10:08:22 -0300

    Adicionando ligacao bidirecional na classe um pra muitos

--- src/modelo/umPraMuitos/Pedido.java
+++ src/modelo/umPraMuitos/Pedido.java
@@ -2,6 +2,7 @@ package modelo.umPraMuitos;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Pedido {
@@ -12,6 +13,10 @@ public class Pedido {
 
     @Column(nullable = false)
     private Date data;
+
+    /*Criando um ligacao bidirecional*/
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
 
     public Pedido() {
         this(new Date());
@@ -36,5 +41,13 @@ public class Pedido {
 
     public void setData(Date data) {
         this.data = data;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
     }
 }
---
+++ src/teste/umPraMuitos/ObterPedido.java
@@ -0,0 +1,20 @@
+package teste.umPraMuitos;
+
+import infra.DAO;
+import modelo.umPraMuitos.ItemPedido;
+import modelo.umPraMuitos.Pedido;
+
+public class ObterPedido {
+
+    public static void main(String[] args) {
+        DAO<Pedido> dao = new DAO<>(Pedido.class);
+
+        Pedido pedido = dao.obterPorId(1L);
+
+        for (ItemPedido item: pedido.getItens()){
+            System.out.println(item. getQuantidade());
+            System.out.println(item.getProduto().getNome());
+        }
+        dao.fechar();
+    }
+}
